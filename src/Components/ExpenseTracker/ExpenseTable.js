36f52b7339209accf59b3e7b1240a8de8c97a8a4
Yash-Bhandari <y.bhandari1@share.epsb.ca>
2020-04-22 14:29:18 -0600

    can sort columns of table but no styling

--- src/Components/ExpenseTracker/ExpenseTable.js
+++ src/Components/ExpenseTracker/ExpenseTable.js
@@ -6,24 +6,53 @@ import TableFooter from '@material-ui/core/TableFooter';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import { DeleteButton } from '../IconButtons';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { formatCost } from '../../utils';
 
 export const ExpenseTable = ({ expenses, deleteExpense }) => {
     const classes = useStyles();
+    const [sortKey, setSortKey] = useState(null);
+    const [sortOrder, setSortOrder] = useState(SortOrder.ASCENDING);
+    const sortedExpenses = sortExpensesBy(expenses, sortKey, sortOrder);
+
+    const toggleSortOrder = () => setSortOrder((sortOrder + 1) % 2);
+
+    const handleHeaderClick = headerName => e => {
+        if (sortKey === headerName) {
+            if (sortOrder === SortOrder.DESCENDING)
+                setSortKey(null);
+            toggleSortOrder();
+        }
+        else
+            setSortKey(headerName);
+    }
     return (
         <Table className={classes.root}>
             <TableHead>
                 <TableRow>
-                    <TableCell>Expense Name</TableCell>
-                    <TableCell align='right'>Cost</TableCell>
-                    <TableCell align='right'>Category</TableCell>
+                    <TableCell
+                        onClick={handleHeaderClick('name')}
+                        hover>
+                        Expense Name
+                    </TableCell>
+                    <TableCell
+                        align='right'
+                        onClick={handleHeaderClick('cost')}
+                        hover>
+                        Cost
+                    </TableCell>
+                    <TableCell
+                        align='right'
+                        onClick={handleHeaderClick('category')}
+                        hover>
+                        Category
+                    </TableCell>
                     <TableCell align='right'>Actions</TableCell>
                 </TableRow>
             </TableHead>
             <TableBody>
-                {expenses.map(expense =>
+                {sortedExpenses.map(expense =>
                     <Expense
                         expense={expense}
                         deleteExpense={deleteExpense}
@@ -58,6 +87,41 @@ const getTotalCost = expenses => expenses.reduce(
     (total, expense) => total + parseFloat(expense.cost),
     0
 )
+
+const sortExpensesBy = (expenses, key, order) => {
+    if (key === null)
+        return expenses;
+
+    const stringCompare = (str1, str2) =>
+        str1 <= str2 ? -1 : 1
+
+    const compareFunction = (expense1, expense2) => {
+        const result = key === ExpenseKeys.COST
+            ? expense1[key] - expense2[key]
+            : stringCompare(expense1[key], expense2[key]);
+
+        return order === SortOrder.ASCENDING
+            ? result
+            : result * -1;
+    }
+    const sortedExpenses = expenses.slice();
+    sortedExpenses.sort(compareFunction);
+    console.log(sortedExpenses)
+    return sortedExpenses;
+}
+
+// enum
+const ExpenseKeys = {
+    COST: 'cost',
+    NAME: 'name',
+    CATEGORY: 'category'
+}
+
+// enum
+const SortOrder = {
+    ASCENDING: 0,
+    DESCENDING: 1
+}
 
 const useStyles = makeStyles(theme => ({
     root: {
